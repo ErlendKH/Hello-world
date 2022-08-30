bbd5cfd3d34137b46e42b364a268050ef4302cc4
ErlendKH <s349571@oslomet.no>
2022-08-30 11:25:03 +0200

    a

---
+++ src/HelloClass.java
@@ -0,0 +1,17 @@
+public class HelloClass {
+
+    //
+    public static void main(String[] args) {
+        System.out.println("TEST START");
+
+//        for(int i = 0; i < 10; i++){
+//            System.out.println(i);
+//        }
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+    }
+
+}
